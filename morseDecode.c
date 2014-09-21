605f35e90e465ea6a38362f4d048bb8c01523c90
Uy Nguyen <nguyenuy@gmail.com>
2014-09-21 13:48:45 -0400

    Add in morse_map - binToChar array

--- morseDecode.c
+++ morseDecode.c
@@ -5,9 +5,66 @@
 #include <asm/uaccess.h>
 #include <linux/vmalloc.h>
 
-int len,temp;
+#define CHAR_SIZE 256
 
+int len,temp;
 char *msg;
+char binToChar[CHAR_SIZE];
+
+void initialize_morse_map() {
+    //dot=1, dash=0
+    binToChar[0b110]    = 'a';
+    binToChar[0b10111]  = 'b';
+    binToChar[0b10101]  = 'c';
+    binToChar[0b1011]   = 'd';
+    binToChar[0b11]     = 'e';
+    binToChar[0b11101]  = 'f';
+    binToChar[0b1001]   = 'g';
+    binToChar[0b11111]  = 'h';
+    binToChar[0b111]    = 'i';
+    binToChar[0b11000]  = 'j';
+    binToChar[0b1010]   = 'k';
+    binToChar[0b11011]  = 'l';
+    binToChar[0b100]    = 'm';
+    binToChar[0b101]    = 'n';
+    binToChar[0b1000]   = 'o';
+    binToChar[0b11001]  = 'p';
+    binToChar[0b10010]  = 'q';
+    binToChar[0b1101]   = 'r';
+    binToChar[0b1111]   = 's';
+    binToChar[0b10]     = 't';
+    binToChar[0b1110]   = 'u';
+    binToChar[0b11110]  = 'v';
+    binToChar[0b1100]   = 'w';
+    binToChar[0b10110]  = 'x';
+    binToChar[0b10100]  = 'y';
+    binToChar[0b10011]  = 'z';
+    binToChar[0b110000] = '1';
+    binToChar[0b111000] = '2';
+    binToChar[0b111100] = '3';
+    binToChar[0b111110] = '4';
+    binToChar[0b111111] = '5';
+    binToChar[0b101111] = '6';
+    binToChar[0b100111] = '7';
+    binToChar[0b100011] = '8';
+    binToChar[0b100001] = '9';
+    binToChar[0b100000] = '0';
+    binToChar[0b1101010] = '.';
+    binToChar[0b1110011] = '?';
+    binToChar[0b1010100] = '!';
+    binToChar[0b101001]  = '(';
+    binToChar[0b1010011] = ')';
+    binToChar[0b1000111] = ':';
+    binToChar[0b101110]  = '=';
+    binToChar[0b1011110] = '-';
+    binToChar[0b1101101] = '"';
+    binToChar[0b1001100] = ',';
+    binToChar[0b1100001] = '\'';
+    binToChar[0b101101]  = '/';
+    binToChar[0b1010101] = ';';
+    binToChar[0b1110010] = '_';
+    binToChar[0b1100101] = '@';
+}
 
 int read_proc(struct file *filp,char *buf,size_t count,loff_t *offp ) 
 {
@@ -43,6 +100,7 @@ void create_new_proc_entry()
 
 
 int proc_init (void) {
+    initialize_morse_map();
     create_new_proc_entry();
     printk(KERN_INFO "Morse code module initialized.");
     return 0;
